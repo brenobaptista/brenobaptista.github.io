ec2263afc2edfbdae188c57911d45696cca48f06
Breno Baptista <brenomb07@hotmail.com>
2020-10-25 00:50:56 -0300

    Updated because of ESLint rules

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,10 @@
-const path = require('path');
-const { createFilePath } = require('gatsby-source-filesystem');
+const path = require('path')
+const { createFilePath } = require('gatsby-source-filesystem')
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions;
+  const { createPage } = actions
 
-  const blogPost = path.resolve('./src/components/BlogPost.js');
+  const blogPost = path.resolve('./src/components/BlogPost.js')
   return graphql(
     `
       {
@@ -27,14 +27,14 @@ exports.createPages = ({ graphql, actions }) => {
     `,
   ).then((result) => {
     if (result.errors) {
-      throw result.errors;
+      throw result.errors
     }
 
-    const posts = result.data.allMdx.edges;
+    const posts = result.data.allMdx.edges
 
     posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node;
-      const next = index === 0 ? null : posts[index - 1].node;
+      const previous = index === posts.length - 1 ? null : posts[index + 1].node
+      const next = index === 0 ? null : posts[index - 1].node
 
       createPage({
         path: `blog${post.node.fields.slug}`,
@@ -44,24 +44,24 @@ exports.createPages = ({ graphql, actions }) => {
           previous,
           next,
         },
-      });
-    });
+      })
+    })
 
-    return null;
-  });
-};
+    return null
+  })
+}
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions;
+  const { createNodeField } = actions
 
   if (node.internal.type === 'Mdx') {
     const value = createFilePath({
       node, getNode,
-    });
+    })
     createNodeField({
       name: 'slug',
       node,
       value,
-    });
+    })
   }
-};
+}
