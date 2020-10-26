e1c971506f458eb1c7f6cb7ecc939df14b49ff30
Breno Baptista <brenomb07@hotmail.com>
2020-10-26 00:46:56 -0300

    Fixed logo test

--- __tests__/logo.js
+++ __tests__/logo.js
@@ -2,14 +2,8 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import Logo from '../src/components/Navbar/Logo'
 
-test('matches snapshot with desktop logo', () => {
+test('matches snapshot', () => {
   const { asFragment } = render(<Logo />)
 
   expect(asFragment(<Logo />)).toMatchSnapshot()
-})
-
-test('matches snapshot with mobile logo', () => {
-  const { asFragment } = render(<Logo mobile />)
-
-  expect(asFragment(<Logo mobile />)).toMatchSnapshot()
 })
